e6981a8008856292f6eaae74322699220599f9b7
Nanderty <psopka@sopka.ch>
2023-10-16 22:10:26 +0200

    new shiny git plugin

--- lua/plugins/general.lua
+++ lua/plugins/general.lua
@@ -8,19 +8,22 @@ return {
 			require('lint').linters_by_ft = {
 				tex = { 'lacheck', }
 			}
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			vim.api.nvim_create_autocmd({ 'BufWritePost' }, {
 				callback = function()
-					require("lint").try_lint()
+					require('lint').try_lint()
 				end,
 			})
 		end,
 	},
 	{
-		'tpope/vim-fugitive',
-		event = {
-			'BufReadPost',
-			'BufNewFile'
+		'NeogitOrg/neogit',
+		keys = {
+			{ '<leader>a', function() require('neogit').open() end, desc = 'git' },
 		},
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+		},
+	  config = true
 	},
 	{
 		'ludovicchabant/vim-gutentags',
