9c1e8d2880e8d0ddac0d9860a977c1ef345acb35
Nanderty <psopka@sopka.ch>
2024-01-31 15:59:19 +0100

    Fixed treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,6 +9,10 @@ return {
 		opts = {
 		    auto_install = true,
 		    ignore_install = { 'latex' },
+		    highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
 		    indent = { enable = true, disable = { 'python' } },
 		    ensure_installed = {
 			'javascript',
@@ -21,6 +25,9 @@ return {
 			'css',
 		    },
 		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
     },
 	{
 		'luckasRanarison/tree-sitter-hyprlang',
