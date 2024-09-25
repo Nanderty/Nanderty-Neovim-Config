640d77514309478ff61e60ba69b45f25dde738a6
Nanderty <psopka@sopka.ch>
2024-09-25 22:49:17 +0200

    improve cursor line nmbr, remove monokai

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,39 +1,4 @@
 return {
-	--{
-	--	'loctvl842/monokai-pro.nvim',
-	--	lazy = false,
-	--	priority = 1000,
-	--	config = function()
-	--		vim.opt.background = 'dark'
-	--		require('monokai-pro').setup({
-	--			override = function(c)
-	--				return {
-	--					TelescopeSelection = { bg = c.base.dimmed3 },
-	--					TelescopePromptNormal = { bg = c.base.dimmed3, fg = c.base.white },
-	--					TelescopePromptBorder = { bg = c.base.dimmed3, fg = c.base.dimmed3 },
-	--					TelescopeResultsNormal = { bg = c.base.dimmed5, fg = c.base.white },
-	--					TelescopeResultsBorder = { bg = c.base.dimmed5, fg = c.base.dimmed5 },
-	--					TelescopePreviewNormal = { bg = c.base.dimmed5 },
-	--					TelescopePreviewBorder = { bg = c.base.dimmed5, fg = c.base.dimmed5 },
-
-	--					TelescopePreviewDirectory = {
-	--						bg = c.prompt_bg,
-	--						fg = c.base.white,
-	--					},
-	--					TelescopePreviewDate = {
-	--						bg = c.prompt_bg,
-	--						fg = c.base.red,
-	--					},
-	--					TelescopePreviewHyphen = {
-	--						bg = c.prompt_bg,
-	--						fg = c.base.white,
-	--					},
-	--				}
-	--			end,
-	--		})
-	--		vim.cmd.colorscheme('monokai-pro')
-	--	end,
-	--},
 	{
 		"yorik1984/newpaper.nvim",
 		lazy = false,
@@ -44,6 +9,7 @@ return {
 				linenumber_bg = colors.bg
 			}
 			vim.g.newpaper_custom_highlights = {
+				CursorLineNr = { bg = colors.bg, fg = colors.cursor_nr_fg },
 				TelescopePromptTitle = { bg = colors.telescope_fg, fg = colors.lightorange },
 				TelescopePromptPrefix = { fg = colors.lightorange },
 				TelescopePromptCounter = { fg = colors.lightorange },
